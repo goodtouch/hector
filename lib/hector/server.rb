3e7a6c605b4bfed025987b631ee2134febcf8c32
Jean-Paul Bonnetouche <jean-paul.bonnetouche@mobile-devices.fr>
2011-02-08 12:07:11 +0100

    Allow configuration of port and ssl_port in config
    ie. in init.rb:
    
    Hector.port = 6667
    Hector.ssl_port = 9999

--- lib/hector/server.rb
+++ lib/hector/server.rb
@@ -1,8 +1,8 @@
 module Hector
   class << self
-    attr_accessor :server_name
+    attr_accessor :server_name, :port, :ssl_port, :motd
 
-    def start_server(address = "0.0.0.0", port = 6767, ssl_port = 6868)
+    def start_server(address = "0.0.0.0", port = self.port, ssl_port = self.ssl_port)
       EventMachine.start_server(address, port, Connection)
       EventMachine.start_server(address, ssl_port, SSLConnection)
       logger.info("Hector running on #{address}:#{port}")
@@ -11,4 +11,6 @@ module Hector
   end
 
   self.server_name = "hector.irc"
+  self.port = 6767
+  self.ssl_port = 6868
 end
